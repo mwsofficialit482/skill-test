a756012983863874b1d9ac66133d37f0cfc0d849
mwsofficialit482 <codewaqasit482@gmail.com>
2025-05-07 20:25:18 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.col-7').classList.add('animate-up');
+    document.querySelector('.col-5').classList.add('animate-down');
+});
